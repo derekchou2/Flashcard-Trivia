ed244b73776295212ef92eaed46a5b5985cc8bed
derekchou2 <59002482+derekchou2@users.noreply.github.com>
2020-08-17 18:40:26 -0400

    Header Bar to Generate New Questions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import FlashcardList from './FlashcardList';
 import './styles.css'
 import axios from 'axios'
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+  const [flashcards, setFlashcards] = useState([])
+  const [categories, setCategories] = useState([])
+
+  const categoryEl = useRef()
+  const amountEl = useRef()
+  const difficultyEl = useRef()
 
   useEffect(() => {
-    axios.get('https://opentdb.com/api.php?amount=10')
+    axios.get("https://opentdb.com/api_category.php")
+    .then(res => {
+      setCategories(res.data.trivia_categories)
+    })
+  }, [])
+
+  function decodeString(str) {
+    const textArea = document.createElement('textarea')
+    textArea.innerHTML = str
+    return textArea.value
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    axios.get('https://opentdb.com/api.php', {
+      params: {
+        difficulty: difficultyEl.current.value,
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
     .then(res => {
       setFlashcards(res.data.results.map((questionItem, index) => {
         const answer = decodeString(questionItem.correct_answer)
@@ -25,43 +50,40 @@ function App() {
       }))
       console.log(res.data)
     })
-  }, []) 
-
-  function decodeString(str) {
-    const textArea = document.createElement('textarea')
-    textArea.innerHTML = str
-    return textArea.value
   }
 
   return (
-   <FlashcardList flashcards = {flashcards}/>
+    <div>
+      <form className = "header" onSubmit = {handleSubmit}>
+        <div className = "form-group"> 
+          <label htmlFor = "category">Category</label>
+          <select id = "category" ref = {categoryEl}>
+            {categories.map(category => {
+              return <option value = {category.id} key = {category.id}>{category.name}</option>
+            })}
+          </select>
+        </div>
+        <div className = "form-group">
+          <label htmlFor = "difficulty">Difficulty</label>
+          <select id = "difficulty" ref = {difficultyEl}>
+            <option value = "easy">Easy</option>
+            <option value = "medium">Medium</option>
+            <option value = "hard">Hard</option>
+          </select>
+        </div>
+        <div className = "form-group">
+          <label htmlFor = "amount">Number of Questions</label>
+          <input type = "number" id = "amount"
+           min = "1" max = "50" step = "1" defaultValue = {10} ref = {amountEl}/>
+        </div>
+        <div className = "form-group">
+          <button className = "button">New Questions</button>
+        </div>
+      </form>
+
+      <FlashcardList flashcards = {flashcards}/>
+   </div>
   );
 }
-
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "what is 2 + 2?",
-    answer: '4',
-    options: [
-      '4',
-      '5',
-      '6',
-      '7'
-    ]
-  },
-  {
-    id: 2,
-    question: "what is the capital of America?",
-    answer: 'Washington DC',
-    options: [
-      'Washington DC',
-      'Africa',
-      'NYC',
-      'Boston'
-    ]
-  }
-]
 
 export default App;
